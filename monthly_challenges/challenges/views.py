8b05c7e0b1ab45616d839355de3fe302314925a0
ggroshansii <garthgroshansii@gmail.com>
2022-04-12 21:41:05 -0400

    months in object; try/except in monthly_challenge view

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -4,30 +4,23 @@ from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
+months = {
+    "january": "this is january",
+    "february": "this is february",
+    "march": "this is march",
+    "april": "this is april",
+    "may": "this is may",
+    "june": "this is june",
+    "july": "this is july",
+    "august": "this is august",
+    "september": "this is september",
+    "october": "this is october",
+    "november": "this is november",
+    "december": "this is decemberß"
+}
+
 def monthly_challenge(request, month):
-    text = None
-    if month == "january":
-       text = "JANUARY" 
-    elif month == "february":
-        text = "FEBRUARY" 
-    elif month == "march":
-        text = "MARCH" 
-    elif month == "april":
-        text = "APRIL" 
-    elif month == "may":
-        text = "MAY" 
-    elif month == "june":
-        text = "JUNE" 
-    elif month == "july":
-        text = "JULY" 
-    elif month == "august":
-        text = "AUGUST" 
-    elif month == "september":
-        text = "SEPTEMBER" 
-    elif month == "november":
-        text = "NOVEMBER" 
-    elif month == "december":
-        text = "DECEMBER" 
-    else:
+    try:
+        return HttpResponse(months[month])
+    except:
         return HttpResponseNotFound("This month not supported")
-    return HttpResponse(text)
