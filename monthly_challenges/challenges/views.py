a553e8695fe5970cf7eb8640ba746935346f4aa0
ggroshansii <garthgroshansii@gmail.com>
2022-04-14 08:54:39 -0400

    monthly_challenge_number view finds correct month but converting the keys of the objects into a list, finding the correct month using number input (index) and using the correct month to access the value in the dict

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,44 +1,34 @@
-from operator import truediv
 from re import I
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-months = [
-    {"january": "this is january"},
-   { "february": "this is february"},
-    {"march": "this is march"},
-   { "april": "this is april"},
-    {"may": "this is may"},
-    {"june": "this is june"},
-    {"july": "this is july"},
-    {"august": "this is august"},
-    {"september": "this is september"},
-    {"october": "this is october"},
-    {"november": "this is november"},
-    {"december": "this is december"},
-]
+months = {
+    "january": "this is january",
+    "february": "this is february",
+    "march": "this is march",
+    "april": "this is april",
+    "may": "this is may",
+    "june": "this is june",
+    "july": "this is july",
+    "august": "this is august",
+    "september": "this is september",
+    "october": "this is october",
+    "november": "this is november",
+    "december": "this is december"
+}
 
 def monthly_challenge_number(request, month):
-    test = month - 1
     try:
-        return HttpResponse(months[test])
+        keys = list(months.keys())
+        month_index = keys[month-1]
+        return HttpResponse(months[month_index])
     except:
         return HttpResponseNotFound("This month not supported")
 
 def monthly_challenge_string(request, month):
-    
-    def filter_month(a_month):
-        print("fired")
-        if a_month[month]: 
-            return True
-        else:
-            return False
-    # try:
-    month_filter = list(filter(filter_month, months))
-
-    print()
-    return HttpResponse("month_filteHr")
-    # except:
-    #     return HttpResponseNotFound("This month not supported")
+    try:
+        return HttpResponse(months[month])
+    except:
+        return HttpResponseNotFound("This month not supported")
